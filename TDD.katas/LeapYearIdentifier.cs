c6c38559cf70118c78c30fa96ed76ead37627f79
Jay <Jayraj@ash-software.com>
2018-12-17 22:01:43 +0000

    added leap year file

---
+++ TDD.katas/LeapYearIdentifier.cs
@@ -0,0 +1,31 @@
+﻿namespace TDD.katas
+{
+    public class LeapYearIdentifier
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if (ATypicalLeap(year))
+                return true;
+
+            if (ATypicalCommon(year))
+                return false;
+
+            return CommonLeap(year);
+        }
+
+        private static bool CommonLeap(int year)
+        {
+            return year % 4 == 0;
+        }
+
+        private static bool ATypicalCommon(int year)
+        {
+            return year % 100 == 0;
+        }
+
+        private static bool ATypicalLeap(int year)
+        {
+            return year % 400 == 0;
+        }
+    }
+}
---
+++ TDD.katas/LeapYearTest.cs
@@ -0,0 +1,52 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace TDD.katas
+{
+    public class LeapYearTest
+    {
+        [Theory]
+        [InlineData(1996)]
+        [InlineData(2008)]
+        [InlineData(2004)]
+        public void LeapYear(int year)
+        {
+            var isLeapYear = LeapYearIdentifier.IsLeapYear(year);
+            isLeapYear.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData(2001)]
+        [InlineData(2002)]
+        [InlineData(2003)]
+
+        public void CommonYear(int year)
+        {
+            var isLeapYear = LeapYearIdentifier.IsLeapYear(year);
+            isLeapYear.ShouldBeFalse();
+        }
+
+
+        [Theory]
+        [InlineData(1900)]
+        [InlineData(100)]
+        [InlineData(200)]
+        public void ATypicalCommonYear(int year)
+        {
+            var isLeapYear = LeapYearIdentifier.IsLeapYear(year);
+            isLeapYear.ShouldBeFalse();
+        }
+
+
+        [Theory]
+        [InlineData(2000)]
+        [InlineData(400)]
+        [InlineData(800)]
+        public void ATypicalLeapYear(int year)
+        {
+            var isLeapYear = LeapYearIdentifier.IsLeapYear(year);
+            isLeapYear.ShouldBeTrue();
+        }
+
+    }
+}
