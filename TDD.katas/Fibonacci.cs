e65b72d5afa07589f8d652f65a0f04762db2f18a
Jay <Jayraj@ash-software.com>
2019-01-01 13:32:53 +0000

    added failing test case

---
+++ TDD.katas/Fibonacci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD.katas
+{
+   public class Fibonacci
+    {
+
+        public static int bonacciGenerator(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDD.katas/FibonacciTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using  Shouldly;
+using Xunit;
+
+namespace TDD.katas
+{
+   public class FibonacciTests
+    {
+        [Fact]
+        public void testNthFibonacci()
+        {
+            var resultedfibonaccinumbers = Fibonacci.bonacciGenerator(0);
+            resultedfibonaccinumbers.ShouldBe(0);
+
+
+        }
+    }
+}
